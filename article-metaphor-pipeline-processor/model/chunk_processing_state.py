bdc12dc43ba85adc24afa1a85b0dd3e57eb9196a
NenadPantelic <nenadpantelickg@example.com>
2026-07-26 15:55:47 +0200

    Implement AnalysisResultAssemblyProcessor

--- article-metaphor-pipeline-processor/model/chunk_processing_state.py
+++ article-metaphor-pipeline-processor/model/chunk_processing_state.py
@@ -15,7 +15,7 @@ class ChunkProcessingError:
 
 @dataclass
 class ChunkProcessingState:
-    _id: str
+    #_id: str
     chunk_id: str
     last_executed_milestone: ProcessingMilestone = None
     last_execution_timestamp: datetime = None
@@ -26,7 +26,7 @@ class ChunkProcessingState:
     def __init__(self, _id: ObjectId = None, chunk_id: str = "", last_executed_milestone: ProcessingMilestone = None,
                  last_execution_timestamp: datetime = None, failed_on_last_execution: bool = False,
                  data: dict[str, dict] = None, errors: list[ChunkProcessingError] = None):
-        self._id = str(_id) if _id else None
+        # self._id = str(_id) if _id else None
         self.chunk_id = chunk_id
         self.last_executed_milestone = last_executed_milestone
         self.last_execution_timestamp = last_execution_timestamp
@@ -44,7 +44,7 @@ class ChunkProcessingState:
             "errors": self.errors,
         }
 
-        if self._id:
-            serialized["_id"] = self._id
+        # if self._id:
+        #     serialized["_id"] = self._id
 
         return serialized
